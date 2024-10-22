7d8d0f5f1d64424daed540c60799e37dfc6a5045
Vinex7 <thabelosilima@gmail.com>
2024-10-22 22:52:50 +0200

    Models and Server files

---
+++ hazon-vinex-backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/hazonvinex', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Sample route
+app.get('/', (req, res) => {
+    res.send('Hazon Vinex API');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
